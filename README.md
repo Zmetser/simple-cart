e16da0da4c6cb0e023ba39047b1a3a430505918b
Oliver Kovacs <zmetser@gmail.com>
2022-02-04 19:19:27 +0100

    exercise boilerplate

---
+++ README.md
@@ -0,0 +1,8 @@
+Feladatok
+ - Kedvencekbe rakas
+ - Kosarba rakas
+ - Kosar update
+ - Kosarbol torles
+ - Mukodo csillagozas
+ - Szallitas modositsa az arat
+ - Promo support
---
+++ js/data.js
@@ -0,0 +1,73 @@
+export default [{
+    id: 'apple-ipad',
+    name: 'Apple iPad',
+    price: 369,
+    image: 'ipad.jpeg',
+  },
+  {
+    id: 'sony-headphone',
+    name: 'Sony Headphone',
+    price: 23,
+    image: 'headphone.jpeg',
+  },
+  {
+    id: 'macbook-air',
+    name: 'Macbook Air',
+    price: 649,
+    image: 'macbook-air',
+  },
+  {
+    id: 'nikon-dslr',
+    name: 'Nikon DSLR',
+    price: 250,
+    image: 'nikon.jpeg',
+  },
+  {
+    id: 'sony-play-station',
+    name: 'Sony Play Station',
+    price: 269,
+    image: 'play-station',
+  },
+  {
+    id: 'macbook-pro',
+    name: 'Macbook Pro',
+    price: 869,
+    image: 'macbook-pro',
+  },
+  {
+    id: 'bose-speaker',
+    name: 'Bose Speaker',
+    price: 99,
+    image: 'speaker.jpeg',
+  },
+  {
+    id: 'samsung-galaxy-s8',
+    name: 'Samsung Galaxy S8',
+    price: 569,
+    image: 'galaxy.jpeg',
+  },
+  {
+    id: 'apple-iphone',
+    name: 'Apple iPhone',
+    price: 349,
+    image: 'iphone.jpeg',
+  },
+  {
+    id: 'canon-dslr',
+    name: 'Canon DSLR',
+    price: 250,
+    image: 'canon.jpeg',
+  },
+  {
+    id: 'google-pixel',
+    name: 'Google Pixel',
+    price: 418,
+    image: 'pixel.jpeg',
+  },
+  {
+    id: 'apple-watch',
+    name: 'Apple Watch',
+    price: 330,
+    image: 'watch.jpeg',
+  }
+]
