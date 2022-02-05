2c433faebb584c86e6a87f19b2a1f0b2c81513c2
Oliver Kovacs <zmetser@gmail.com>
2022-02-05 17:31:54 +0100

    update total price after cart re-render

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,9 @@
+import { getProductById } from './products.js';
 
 class Cart{
     constructor(qty){
         this.products = [];
-    }  
+    }
     addToCart(id){
         const cartItem = {
             id,
@@ -11,10 +12,27 @@ class Cart{
         const prod = this.products.find( prod => prod.id === cartItem.id )
         if (!prod) {
             this.products.push(cartItem);
-           
+
         } else {
             prod.qty ++;
-        } 
+        }
+    }
+
+    // getTotalPrice() {
+    //   let totalPrice = 0;
+    //   // { id: , qty: }
+    //   this.products.forEach(({ id, qty }) => {
+    //     const realProduct = getProductById(id);
+    //     totalPrice += realProduct.price * qty
+    //   });
+    //   return totalPrice;
+    // }
+
+    getTotalPrice() {
+      return this.products.reduce((totalPrice, { id, qty }) => {
+        const realProduct = getProductById(id);
+        return totalPrice + realProduct.price * qty;
+      }, 0);
     }
 }
 
