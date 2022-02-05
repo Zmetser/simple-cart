0af5989e2aa2a92c3cb2ecf1d68b884c1b823c1a
KTomi <tkiss002@gmail.com>
2022-02-05 15:14:24 +0100

    TomiCart

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ fav.forEach(wishI => wishI.addEventListener('click', (event) => {
     event.target.classList.toggle("fa-heart-o", !isFavProduct)
 
 }));
-
+const cartItem = document.querySelector(".cart")
 const btns = document.querySelectorAll('.btn');
 const cart = new Cart();
 btns.forEach((button)=>{
@@ -23,5 +23,16 @@ btns.forEach((button)=>{
         cart.addToCart(id)
         console.log(cart)
         console.log(getProductById(id))
+        console.log(getProductById(id).image);
+        cartItem.innerHTML+=`
+        <div class="row border-top border-bottom">
+                  <div class="row main align-items-center">
+                      <div class="col-2"><img class="img-fluid" src="./images/${getProductById(id).image}"></div>
+                      <div class="col">
+                          <div class="row">${getProductById(id).name}</div>
+                      </div>
+                      <div class="col"><input type="number" value="1"></div>
+                      <div class="col">${getProductById(id).price}$ <a href="" class="close">&#10005;</a></div>
+                  </div>`
     })
 })
--- js/cart.js
+++ js/cart.js
@@ -15,6 +15,7 @@ class Cart{
             prod.qty ++
         } 
     }
+
 }
 
 export { Cart }
