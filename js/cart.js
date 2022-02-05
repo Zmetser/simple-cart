fc4ef1b4e72336047d31d084a2943e60df946c75
Pista_Laszlo <pistalaszlo@ymail.com>
2022-02-05 14:11:46 +0000

    corrected lines after first pull request, let is now const, == is now ===

--- js/cart.js
+++ js/cart.js
@@ -32,41 +32,41 @@ function renderCart(cart){
     clearCart()
    
     cart.products.forEach((items)=>{
-        const productfind = data.find(dbId => dbId.id == items.id)
-        let newRow = document.createElement('div')
+        const productfind = data.find(dbId => dbId.id === items.id)
+        const newRow = document.createElement('div')
         newRow.classList.add('row', 'border-top', 'border-bottom', 'cartItem')
         
 
-        let mainRow = document.createElement('div')
+        const mainRow = document.createElement('div')
         mainRow.classList.add('row', 'main', 'align-items-center')
         newRow.append(mainRow)
         //img append
-        let imgDiv = document.createElement('div')
+        const imgDiv = document.createElement('div')
         imgDiv.classList.add('col-2')
         
-        let itemIMG = document.createElement('img')
+        const itemIMG = document.createElement('img')
         itemIMG.classList.add('img-fluid')
         itemIMG.src = `./images/${productfind.image}`
         imgDiv.append(itemIMG)
         //description append
-        let dContainer = document.createElement('div')
+        const dContainer = document.createElement('div')
         dContainer.classList.add('col')
-        let descriptionOfProduct = document.createElement('div')
+        const descriptionOfProduct = document.createElement('div')
         descriptionOfProduct.classList.add('row')
         descriptionOfProduct.innerText = productfind.name
         dContainer.append(descriptionOfProduct)
         //qty input
-        let dContainer2 = document.createElement('div')
+        const dContainer2 = document.createElement('div')
         dContainer2.classList.add('col')
-        let qtyInput = document.createElement('input')
+        const qtyInput = document.createElement('input')
         qtyInput.type = "number";
         qtyInput.value = items.qty;
         dContainer2.append(qtyInput)
         //close cross
-        let dContainer3 = document.createElement('div');
+        const dContainer3 = document.createElement('div');
         dContainer3.classList.add('col')
         dContainer3.innerText = `$${productfind.price}`
-        let close = document.createElement('a')
+        const close = document.createElement('a')
         close.classList.add('close')
         close.innerHTML = '&#10005;'
         dContainer3.append(close)
