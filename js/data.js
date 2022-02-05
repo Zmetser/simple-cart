69f463cc2a2d0da5c01bd2c41e88a8e3bb1ad9fc
Hatala Mark <hatala.mark@gmail.com>
2022-02-05 15:17:07 +0100

    update data

--- js/data.js
+++ js/data.js
@@ -2,13 +2,13 @@ export default [{
     id: 'apple-ipad',
     name: 'Apple iPad',
     price: 369,
-    image: 'ipad.jpeg',
+    image: 'ipad',
   },
   {
     id: 'sony-headphone',
     name: 'Sony Headphone',
     price: 23,
-    image: 'headphone.jpeg',
+    image: 'headphone',
   },
   {
     id: 'macbook-air',
@@ -20,7 +20,7 @@ export default [{
     id: 'nikon-dslr',
     name: 'Nikon DSLR',
     price: 250,
-    image: 'nikon.jpeg',
+    image: 'nikon',
   },
   {
     id: 'sony-play-station',
@@ -38,36 +38,36 @@ export default [{
     id: 'bose-speaker',
     name: 'Bose Speaker',
     price: 99,
-    image: 'speaker.jpeg',
+    image: 'speaker',
   },
   {
     id: 'samsung-galaxy-s8',
     name: 'Samsung Galaxy S8',
     price: 569,
-    image: 'galaxy.jpeg',
+    image: 'galaxy',
   },
   {
     id: 'apple-iphone',
     name: 'Apple iPhone',
     price: 349,
-    image: 'iphone.jpeg',
+    image: 'iphone',
   },
   {
     id: 'canon-dslr',
     name: 'Canon DSLR',
     price: 250,
-    image: 'canon.jpeg',
+    image: 'canon',
   },
   {
     id: 'google-pixel',
     name: 'Google Pixel',
     price: 418,
-    image: 'pixel.jpeg',
+    image: 'pixel',
   },
   {
     id: 'apple-watch',
     name: 'Apple Watch',
     price: 330,
-    image: 'watch.jpeg',
+    image: 'watch',
   }
 ]
