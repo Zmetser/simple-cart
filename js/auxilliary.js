a5d7553c94da3e8dcd164704ca467720b3c849f0
Dávid Végh <davidvegh94@gmail.com>
2022-02-05 16:08:39 +0100

    szopas

--- js/auxilliary.js
+++ js/auxilliary.js
@@ -1,5 +1,6 @@
 import data from './data.js';
-import {cart} from './app.js'
+import { cart } from './app.js'
+import { getProductById } from './products.js';
 
 function getProductId(element) {
     let currentElement = element;
@@ -13,27 +14,29 @@ function getProductId(element) {
 
 
 function insertToCart(id) {
-    cart.products.forEach(qtyprod => {
-        if(){
-
+    cart.products.forEach(chosenprod => {
+        if (chosenprod.id === id) {
+            console.log("hello")
         }
-        
+        else {
+            const item = data.find(product => product.id === id)
+            const prod = document.createElement("div");
+            const targetdiv = document.querySelector(".col-md-8.cart")
+            prod.classList.add("row", "border-top", "border-bottom")
+            prod.innerHTML = "szöveg"
+            prod.innerHTML = `<div class="row main align-items-center">
+            <div class="col-2"><img class="img-fluid" src="./images/${item.image}"></div>
+            <div class="col">
+                <div class="row">${item.name}</div>
+            </div>
+            <div class="col"><input type="number" value="1"></div>
+            <div class="col">$${item.price} <a href="" class="close">&#10005;</a></div>
+            </div>`
+            console.log(`ezaz ${prod}`)
+            targetdiv.append(prod)
+        }
     })
-    const item = data.find(product => product.id === id)
-    const prod = document.createElement("div");
-    const targetdiv = document.querySelector(".col-md-8.cart")
-    prod.classList.add("row", "border-top", "border-bottom")
-    prod.innerHTML = "szöveg"
-    prod.innerHTML = `<div class="row main align-items-center">
-    <div class="col-2"><img class="img-fluid" src="./images/${item.image}"></div>
-    <div class="col">
-        <div class="row">${item.name}</div>
-    </div>
-    <div class="col"><input type="number" value="${cart.products[0].qty}"></div>
-    <div class="col">$${item.price} <a href="" class="close">&#10005;</a></div>
-    </div>`
-    console.log(`ezaz ${prod}`)
-    targetdiv.append(prod)
+    
 }
 
 
